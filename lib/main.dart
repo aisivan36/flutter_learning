9e29c43f987d25cd575d00c49b9821dc9b5d4a27
ivan-boginski <aisivan36@gmail.com>
2020-03-22 11:22:46 +0700

    add new code

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,38 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer')
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter Learning'),
         ),
-        body: Text('Just for testing it'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion(),
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
