5a3627035c841220ebff9762d3d2db636ac3f374
ivan-boginski <aisivan36@gmail.com>
2020-03-23 19:41:18 +0700

    Added a new bug and feature's will be fixed as soon as possible

--- lib/main.dart
+++ lib/main.dart
@@ -18,20 +18,45 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': "What's your favorite color?",
-      'answers': ['Black', 'Reed', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Reed', 'score': 6},
+        {'text': 'Green', 'score': 4},
+        {'text': 'White', 'score': 2},
+      ],
     },
     {
       'questionText': "What's your favorite animal?",
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 5},
+        {'text': 'Elephant', 'score': 7},
+        {'text': 'Pussycat', 'score': 10},
+      ],
     },
     {
-      'questionText': 'Whos your favorite instructor',
-      'answers': ['You tube', 'Udemy', 'StackOverflow', 'Quora'],
+      'questionText': 'Who\'s your favorite instructor',
+      'answers': [
+        {'text': 'YouTube', 'score': 1},
+        {'text': 'Udemy', 'score': 1},
+        {'text': 'StackOverflow', 'score': 1},
+        {'text': 'Quora', 'score': 1},
+      ],
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -60,7 +85,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
 
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    var resutlText = 'You did it';
+    if (resultScore <= 8) {
+      resutlText = 'Great makes it possible to take any question again';
+    } else if (resultScore <= 12) {
+      resutlText = "Good job";
+    } else if (resultScore <= 16) {
+      resutlText = 'You\'re very excited';
+    } else {
+      resutlText = 'It\'s okay we\'ll try again';
+    }
+    return resutlText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it'),
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(
+                fontSize: 37,
+                fontWeight: FontWeight.w600,
+                color: Colors.purpleAccent),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetHandler,
+            child: Text(
+              'Restart Quiz!',
+            ),
+            textColor: Colors.blue,
+          )
+        ],
+      ),
     );
   }
 }
